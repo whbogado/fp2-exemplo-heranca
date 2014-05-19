579c77f3d43f5d2bfc3cd767c0c7197ae9c76118
Wilson <Wilson@homeserver>
2014-05-19 19:24:08 -0300

    Correções estéticas.

--- src/main/java/ExemploHeranca.java
+++ src/main/java/ExemploHeranca.java
@@ -7,14 +7,14 @@ import utfpr.ct.dainf.if62c.exemplos.TrianguloEquilatero;
  * IF62C Fundamentos de Programação 2
  * Exercício de programação em Java.
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
- */public class ExemploHeranca 
-{
-    public static void main( String[] args )
-    {
+ */
+public class ExemploHeranca {
+
+    public static void main(String[] args) {
         Retangulo r = new Retangulo(4, 2);
         Quadrado q = new Quadrado(4);
         TrianguloEquilatero te = new TrianguloEquilatero(4);
-        
+
         System.out.println("Area do retângulo = " + r.getArea());
         System.out.println("Perímetro do retângulo = " + r.getPerimetro());
         System.out.println("Area do quadrado = " + q.getArea());
